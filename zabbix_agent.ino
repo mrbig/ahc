c2f7b6138794563558261a054a11d4327d312d9b
Nagy Attila Gabor <mrbig@sneaker.hu>
2013-01-24 23:04:14 +0100

    Proto code for checking the agent strings against an array of strings

--- zabbix_agent.ino
+++ zabbix_agent.ino
@@ -2,6 +2,12 @@
  * This file contains the codes of the zabbix agent
  */
 
+static const char cmd1[] PROGMEM = "agent.ping";
+static const char cmd2[] PROGMEM = "agent.version";
+
+static const char* zabbix_commands[] = {cmd1, cmd2};
+
+
 /**
  * Handle incoming requests for the zabbix agent
  * Parse the requested item, and call the appropriate handler
@@ -10,26 +16,74 @@
  * @param word pos position in the tcp packet
  */
 static void serviceZabbixRequest(BufferFiller &buf, word pos) {
+
+  byte i;
+  String cmd = String((char *) Ethernet::buffer + pos);
+  String check;
+  const char *ptr;
+  char c;
+
 #if DSERIAL
   Serial.println("got packet on 10050");
-  char * data = (char *) Ethernet::buffer + pos;
-  Serial.println(data);
+  Serial.println(cmd);
 #endif
-  byte i;
+
+  // 2: this is the size of a pointer
+  for (i=0; i<(sizeof zabbix_commands)/2; i++) {
+    // read from the eeprom
+    check = "";
+    ptr = zabbix_commands[i];
+    while ((c = pgm_read_byte(ptr++))) {
+      check += c;
+    }
+    check += '\n'; // every command ends with a newline
+    // now check contains the command
+    
+    Serial.print(check.length(), DEC);
+    Serial.println(check);
+    if (cmd == check) {
+      Serial.print("matched command ");
+      Serial.println(i, DEC);
+    } else {
+      Serial.println("no match");
+    }
+  }
+  
+
+  sendZabbixResponse(buf, "demo02");
+
+}
+
+/**
+ * Sends a response to the client
+ *
+ * @param BufferFiller buf the buffer filler to be used for output
+ * @param const char* the response to be sent
+ */
+void sendZabbixResponse(BufferFiller &buf, const char* response) {
   char header[9];
-  buf = ether.tcpOffset();
-  buf.emit_p(PSTR("ZBXD"));
+  byte i, len;
+
+  len = strlen(response);
+  
+  // Create the header
   for (i=0; i<9; i++) {
     switch (i) {
       case 0:
         header[i] = 0x01; break;
       case 1:
-        header[i] = 5; break;
+        header[i] = len; break;
       default:
         header[i] = 0;
     }
   }
+  Serial.println("response:");
+  Serial.print(len);
+  Serial.println(response);
+  
+  buf = ether.tcpOffset();
+  buf.emit_p(PSTR("ZBXD"));
   buf.emit_raw(header, 9);
-  buf.emit_p(PSTR("demo1"));
+  buf.emit_raw(response, len);
   ether.httpServerReply(buf.position());
 }
