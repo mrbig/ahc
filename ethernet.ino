bb0b496905cf3fc59881c8ae8b83b16f404c7b99
Nagy Attila Gabor <mrbig@sneaker.hu>
2013-12-17 23:55:18 +0100

    Fixed license text

--- ethernet.ino
+++ ethernet.ino
@@ -2,12 +2,12 @@
  *
  *     This file is part of AHC - AHC Humidity Control.
  *
- *  TrackMii is free software: you can redistribute it and/or modify
+ *  AHC is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  TrackMii is distributed in the hope that it will be useful,
+ *  AHC is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
